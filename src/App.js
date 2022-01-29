8711d6159eb1c5691a6003e4954ed795d4f32409
Magic_Vel <magic.valentyn@gmail.com>
2022-01-29 19:06:54 +0200

    add rxjs

--- src/App.js
+++ src/App.js
@@ -1,76 +1,89 @@
 import React, {useState} from 'react';
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
 import Display from './components/Display';
 import Btn from './components/btn';
 import './App.css';
 
+const stopwatchStepMs = 10;
+const waitClickedInterval = 300;
+
+const notStartedStatus = 0;
+const startedStatus = 1;
+const waitingStatus = 2;
+
 
 function App() {
-  const [time, setTime] = useState({s: 0, m: 0, h: 0});
-  const [stopwatch, setStopwatch] = useState();
-  const [resumeClicked, setResumeClicked] = useState(false);
-  const [status, setStatus] = useState(0);
-  // Statuses: 
-  // not started = 0
-  // started = 1
-  // waite = 2
-
- 
-
-const run = () => {
-  setTime(
-    (time) => {
-      let s = time.s,
-        m = time.m,
-        h = time.h
-      
-      s++;
-      if(s === 60){
-        m++;
-        s = 0;
-      }
-      if(m === 60){
-        h++;
-        m = 0;
-      }
-      return {s: s, m: m, h: h}
-    }
-  )
-};
+  const [time, setTime] = useState({ms:0, s: 0, m: 0, h: 0});
+  const [ticking, setTicking] = useState();
+  const [waitClickedTime, setWaitClickedTime] = useState();
+  const [status, setStatus] = useState(notStartedStatus);
 
   const start = () => {
-    setStatus(1);
-    setStopwatch(setInterval(run, 1000));
+    setStatus(startedStatus);
+    
+    const subscription = new Subject();
+    setTicking(subscription);
+
+    interval(stopwatchStepMs)
+        .pipe(takeUntil(subscription))
+        .subscribe(() => {
+          setTime(
+            (time) => {
+              let ms = time.ms,
+                s = time.s,
+                m = time.m,
+                h = time.h
+              
+              ms += stopwatchStepMs;
+              if (ms === 1000){
+                s++;
+                ms = 0;
+              }
+
+              if(s === 60){
+                m++;
+                s = 0;
+              }
+              if(m === 60){
+                h++;
+                m = 0;
+              }
+              return {ms: ms, s: s, m: m, h: h}
+            }
+          )
+        });
+  };
+
+  const stopTicking = () => {
+    ticking.next();
+    ticking.complete();
   };
 
   const wait = () => {
-    if (resumeClicked){
-      clearInterval(stopwatch);
-      setStatus(2);
+    let now = new Date().getTime();
+    if (waitClickedTime && now - waitClickedTime < waitClickedInterval){
+      stopTicking()
+      setStatus(waitingStatus);
     }
-    setResumeClicked(true);
-
-    setTimeout(() => {
-      setResumeClicked(false);
-    }, 300);
+    setWaitClickedTime(now);
   };
   
   const stop = () => {
-    clearInterval(stopwatch);
-    setStatus(0);
-    setTime({s:0, m:0, h:0})
+    stopTicking()
+    setStatus(notStartedStatus);
+    setTime({ms:0, s: 0, m: 0, h: 0})
   };
 
   const resume = () => start();
 
   const reset = () => {
-    clearInterval(stopwatch);
-    setTime({s:0, m:0, h:0});
+    stop();
     start();
   };
 
   
-
-
   return (
     <div className="main-section">
      <div className="clock-holder">
